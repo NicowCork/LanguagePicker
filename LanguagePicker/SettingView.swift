82b2baea61d0850d22adc15ebac343a128d72e48
Nicolas Richard <nicolas.richard42152@gmail.com>
2024-06-05 00:05:19 +0100

    Hello

---
+++ LanguagePicker/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  LanguagePicker
+//
+//  Created by Nicolas on 04/06/2024.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
